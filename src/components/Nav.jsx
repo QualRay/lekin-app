ab479e3ca81ccec179cf3223e0763e9baee07c47
Ray Rana <shree3ray@gmail.com>
2025-02-04 15:24:50 -0600

    updated nav with category

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
+import { ProductContext } from '../utils/Context';
 
 const Nav = () => {
+
+  const { products } = useContext(ProductContext);
+  //console.log(products);
+  let distinct_category = products && products.reduce((acc, cv) => [...acc, cv.category], []);
+  distinct_category = [...new Set(distinct_category)].map(cat => cat.charAt(0).toUpperCase() + cat.slice(1));
+  //console.log(distinct_category);
+  
+  const color = () => {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    
+    return `rgb(${r},${g},${b})`;
+  }
+
+
   return (
     <>
     <nav className="w-[20%] bg-zinc-50 h-full text-black flex flex-col items-center pt-5">
@@ -12,24 +30,21 @@ const Nav = () => {
       </a>
       <hr className="w-[80%] my-3" />
       <h1 className="text-2xl mb-3 w-[80%] text-center">Category Filter</h1>
-      <ul className="w-[80%] ">
-        <li className="mb-3">
-          <span className="bg-blue-200 px-2 rounded-full mr-2"></span>
-          Cat 1
-        </li>
-        <li className="mb-3">
-          <span className="bg-red-200 px-2 rounded-full mr-2"></span>
-          Cat 2
-        </li>
-        <li className="mb-3">
-          <span className="bg-blue-200 px-2 rounded-full mr-2"></span>
-          Cat 3
-        </li>
-        <li className="mb-3">
-          <span className="bg-red-200 px-2 rounded-full mr-2"></span>
-          Cat 4
-        </li>
-      </ul>
+      <div className="w-[80%] ">
+
+        {distinct_category && distinct_category.map((cat, i) => (
+          
+          <Link
+          key={i} to={`/?category=${cat.toLowerCase()}`} 
+          className="mb-3 flex items-center">
+          <span style={{backgroundColor: color()}} className="px-2 rounded-full mr-2 w-[15px] h-[15px]"></span>{ " "}
+          {cat}
+        </Link>
+
+        ))}
+
+        
+      </div>
     </nav>
     </>
   );
